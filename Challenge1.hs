9eed709c5e48f8b4be953e24b1b9dd32ab254cdb
Jack Palfrey <jack.palf@gmail.com>
2014-10-14 22:22:15 +0100

    updated challenge 01 to use SDL2 and Cove

--- Challenge1.hs
+++ Challenge1.hs
@@ -1,29 +1,105 @@
 import Prelude hiding ((.), id)
 
+import Control.Monad
 import Control.Wire
-import FRP.Netwire
+import Data.Bits
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Utils
+import GHC.Word
+import Graphics.UI.SDL.Types
+import qualified FRP.Netwire as Netwire
 import qualified Graphics.UI.SDL as SDL
 
+
+
+-- challenge1 :: (, Monad m) => Wire s e m Double Double
+challenge1 :: (Monad m, HasTime t s) => Wire s e m Double Double
+challenge1 = Netwire.integral 0 + pure 2
+
+
 main :: IO ()
-main = SDL.withInit [SDL.InitEverything] $ do
-  screen <- SDL.setVideoMode 200 200 32 [SDL.SWSurface]
-  go screen clockSession_ challenge1 0
+main = withSDLWindow ("Challenge 01", 200, 200) $ \renderer ->
+    wireLoop clockSession_ challenge1 0 (drawFunc renderer)
 
- where
 
-  go screen s w x = do
-    (ds, s') <- stepSession s
-    (ex, w') <- stepWire w ds (Right x)
+wireLoop :: (HasTime t s) => Session IO s -> Wire s e IO Double Double -> Double -> (Double -> IO a) -> IO b
+wireLoop session wire x micro = do
+    (ds, session') <- stepSession session
+    (ex, wire') <- stepWire wire ds (Right x)
     let x' = either (const 0) id ex
-    (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 255 255 255 >>=
-        SDL.fillRect screen Nothing
+    micro x'
+    wireLoop session' wire' x' micro
 
-    (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 50 200 >>=
-        SDL.fillRect screen (Just $ SDL.Rect (round x) 0 50 50)
 
-    SDL.flip screen
-    SDL.delay (1000 `div` 60)
-    go screen s' w' x'
 
-challenge1 :: (HasTime t s, Monad m) => Wire s e m Double Double
-challenge1 = integral 0 + 20
+drawFunc :: SDL.Renderer -> Double -> IO ()
+drawFunc renderer x = void $ withBlankScreen renderer (drawSquareAt x 0)
+
+
+
+squareRect :: (Integral a) => a -> SDL.Rect
+squareRect x = SDL.Rect {
+    rectX = fromIntegral x,
+    rectY =  0,
+    rectW = 50,
+    rectH = 50 }
+
+
+drawSquareAt :: Double -> Double -> SDL.Renderer -> IO ()
+drawSquareAt x y renderer = do
+    SDL.setRenderDrawColor renderer 0xFF 0x00 0x00 0xFF
+    void $ with (squareRect (round x)) (SDL.renderFillRect renderer)
+
+
+withBlankScreen :: SDL.Renderer -> (SDL.Renderer -> IO a) -> IO ()
+withBlankScreen renderer operation = do
+    SDL.setRenderDrawColor renderer 0xFF 0xFF 0xFF 0xFF
+    SDL.renderClear renderer
+    operation renderer
+    SDL.renderPresent renderer
+
+
+
+
+
+
+
+
+withSDLWindow :: (String, Int, Int) -> (SDL.Renderer -> IO a) -> IO ()
+withSDLWindow windowData renderOperation = do
+    initializeSDL [SDL.initFlagVideo]
+
+    window <- createWindow windowData
+    renderer <- createRenderer window (-1) [SDL.rendererFlagAccelerated, SDL.rendererFlagPresentVSync]
+
+    renderOperation renderer
+    sdlCleanup window renderer
+
+
+sdlCleanup :: SDL.Window -> SDL.Renderer -> IO ()
+sdlCleanup window renderer = do
+    SDL.destroyRenderer renderer
+    SDL.destroyWindow window
+    SDL.quit
+
+
+initializeSDL :: [Word32] -> IO ()
+initializeSDL flags = void $ SDL.init (bitwiseOr flags)
+
+
+createWindow :: (String, Int, Int) -> IO SDL.Window
+createWindow (windowTitle, windowWidth, windowHeight) = withCAString windowTitle $ \title ->
+    SDL.createWindow title x y w h SDL.windowFlagShown
+    where x = SDL.windowPosUndefined
+          y = SDL.windowPosUndefined
+          w = fromIntegral windowWidth
+          h = fromIntegral windowHeight
+
+
+-- createRenderer :: SDL.Window -> CInt -> [Word32] -> IO SDL.Renderer
+createRenderer window index flags = SDL.createRenderer window index (bitwiseOr flags)
+
+
+bitwiseOr :: [Word32] -> Word32
+bitwiseOr = foldl (.|.) 0
--- Challenge2.hs
+++ Challenge2.hs
@@ -1,18 +1,43 @@
 {-# LANGUAGE StandaloneDeriving #-}
-import Prelude hiding ((.), id, null, filter)
+
 import Control.Monad (void)
 import Control.Wire hiding (empty)
-import FRP.Netwire hiding (empty)
+import Data.Bits
 import Data.Monoid (Monoid)
 import Data.Set (Set, empty, insert, delete, null, filter)
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Utils
+import GHC.Word
+import Graphics.UI.SDL.Types
+import Prelude hiding ((.), id, null, filter)
+import qualified FRP.Netwire as Netwire hiding (empty)
 import qualified Graphics.UI.SDL as SDL
 
-main :: IO ()
-main = SDL.withInit [SDL.InitEverything] $ do
-  screen <- SDL.setVideoMode 200 200 32 [SDL.SWSurface]
-  void $ go empty screen clockSession_ challenge2 0
 
- where
+challenge2 :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym) Double
+challenge2 = Netwire.integral 0 . challenge2_velocity
+
+
+challenge2_velocity :: (Monad m, Monoid e) => Wire s e m (Set SDL.Keysym) Double
+challenge2_velocity  =  pure (-20) . when (keyDown SDL.SDLK_LEFT)
+                    <|> pure 20 . when (keyDown SDL.SDLK_RIGHT)
+                    <|> pure 0
+
+
+main :: IO ()
+main = withSDLWindow ("Challenge 02", 200, 200) $ do \renderer ->
+    wireLoop clockSession challenge2 0 (drawFunc renderer)
+
+
+wireLoop :: (HasTime t s) => Session IO s -> Wire s e IO Double Double -> Double -> (Double -> IO a) -> IO b
+wireLoop session wire x micro = do
+    (ds, session') <- stepSession session
+    (ex, wire') <- stepWire wire ds (Right x)
+    let x' = either (const 0) id ex
+    micro x'
+    wireLoop session' wire' x' micro
+
 
   go keysDown screen s w x = do
     keysDown' <- parseEvents keysDown
@@ -30,13 +55,7 @@ main = SDL.withInit [SDL.InitEverything] $ do
     SDL.delay (1000 `div` 60)
     go keysDown' screen s' w' x'
 
-challenge2 :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym) Double
-challenge2 = integral 0 . challenge2_velocity
 
-challenge2_velocity :: (Monad m, Monoid e) => Wire s e m (Set SDL.Keysym) Double
-challenge2_velocity  =  pure (-20) . when (keyDown SDL.SDLK_LEFT)
-                    <|> pure 20 . when (keyDown SDL.SDLK_RIGHT)
-                    <|> pure 0
 
 parseEvents :: Set SDL.Keysym -> IO (Set SDL.Keysym)
 parseEvents keysDown = do
@@ -51,3 +70,78 @@ keyDown :: SDL.SDLKey -> Set SDL.Keysym -> Bool
 keyDown k = not . null . filter ((== k) . SDL.symKey)
 
 deriving instance Ord SDL.Keysym
+
+
+
+
+
+drawFunc :: SDL.Renderer -> Double -> IO ()
+drawFunc renderer x = void $ withBlankScreen renderer (drawSquareAt x 0)
+
+
+
+squareRect :: (Integral a) => a -> SDL.Rect
+squareRect x = SDL.Rect {
+    rectX = fromIntegral x,
+    rectY =  0,
+    rectW = 50,
+    rectH = 50 }
+
+
+drawSquareAt :: Double -> Double -> SDL.Renderer -> IO ()
+drawSquareAt x y renderer = do
+    SDL.setRenderDrawColor renderer 0xFF 0x00 0x00 0xFF
+    void $ with (squareRect (round x)) (SDL.renderFillRect renderer)
+
+
+withBlankScreen :: SDL.Renderer -> (SDL.Renderer -> IO a) -> IO ()
+withBlankScreen renderer operation = do
+    SDL.setRenderDrawColor renderer 0xFF 0xFF 0xFF 0xFF
+    SDL.renderClear renderer
+    operation renderer
+    SDL.renderPresent renderer
+
+
+
+
+
+
+
+
+withSDLWindow :: (String, Int, Int) -> (SDL.Renderer -> IO a) -> IO ()
+withSDLWindow windowData renderOperation = do
+    initializeSDL [SDL.initFlagVideo]
+
+    window <- createWindow windowData
+    renderer <- createRenderer window (-1) [SDL.rendererFlagAccelerated, SDL.rendererFlagPresentVSync]
+
+    renderOperation renderer
+    sdlCleanup window renderer
+
+
+sdlCleanup :: SDL.Window -> SDL.Renderer -> IO ()
+sdlCleanup window renderer = do
+    SDL.destroyRenderer renderer
+    SDL.destroyWindow window
+    SDL.quit
+
+
+initializeSDL :: [Word32] -> IO ()
+initializeSDL flags = void $ SDL.init (bitwiseOr flags)
+
+
+createWindow :: (String, Int, Int) -> IO SDL.Window
+createWindow (windowTitle, windowWidth, windowHeight) = withCAString windowTitle $ \title ->
+    SDL.createWindow title x y w h SDL.windowFlagShown
+    where x = SDL.windowPosUndefined
+          y = SDL.windowPosUndefined
+          w = fromIntegral windowWidth
+          h = fromIntegral windowHeight
+
+
+-- createRenderer :: SDL.Window -> CInt -> [Word32] -> IO SDL.Renderer
+createRenderer window index flags = SDL.createRenderer window index (bitwiseOr flags)
+
+
+bitwiseOr :: [Word32] -> Word32
+bitwiseOr = foldl (.|.) 0
