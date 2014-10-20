3f10b784a0b33cac86a1fdc185f9b845a781d2f2
Jack Palfrey <jack.palf@gmail.com>
2014-10-20 11:38:45 +0100

    completed challenge03; collision and bounce occurs at 50px and 150px

--- src/challenge03.hs
+++ src/challenge03.hs
@@ -10,9 +10,6 @@ import FRP.Netwire
 import Prelude hiding ((.), id, null, filter)
 
 
-import qualified Graphics.UI.SDL as SDL
-
-
 type DiffTime = Timed NominalDiffTime ()
 
 
@@ -25,9 +22,8 @@ wireLoop session wire x micro = do
     wireLoop session' wire' x' micro
 
 
-drawWith :: forall a. (RealFrac a, Show a) => SDL.Renderer -> a -> IO ()
+-- drawWith :: forall a. (RealFrac a, Show a) => SDL.Renderer -> a -> IO ()
 drawWith renderer x = do
-    print x
     runRender (drawSquareAt x' 100) renderer
     where x' = round x :: Int
 
@@ -38,82 +34,46 @@ drawSquareAt x y = do
     drawRect (x - 25) (y - 25) 50 50
 
 
-
-collided :: (Ord a, Num a) => (a, a) -> a -> (a, Bool)
-collided (a, b) x
-  | x < a = (a, True)
-  | x > b = (b, True)
-  | otherwise = (x, False)
-
-
--- position :: (Monad m, HasTime t s) => Wire s e m Double (Double, Bool)
--- position = integral 0 >>> (arr $ collided (0, 150))
-
--- arr $ collided (0, 150) :: Arrow ? a (a, Bool)
-
-
-position = integralWith' clamp 0
-    where
-        clamp p | p < 0 || p > 150 = (max 1 (min 149 p), True)
-                | otherwise        = (p, False)
-
-
-dampen :: (Fractional a, HasTime t s) => a -> Wire s e m a a
-dampen rate = integralWith' (rate *) 0
-
-
-constrain :: Double -> Double -> Double -> Double
-constrain lower upper x
-    | lower > upper = constrain upper lower x
-    | x < lower     = lower
-    | x > upper     = upper
-    | otherwise     = x
-
-
-limit :: Double -> Double -> Wire DiffTime () IO Double Double
-limit lower upper = mkSF_ $ constrain lower upper
-
-
-type Stuff = Int
-
-acceleration :: forall a s. Wire s () IO Stuff Double
+acceleration :: Wire s () IO a Double
 acceleration = pure (-200) . isKeyDown LeftKey
     <|> pure 200 . isKeyDown RightKey
     <|> pure 0
 
 
-velocity :: forall a. Wire DiffTime () IO (Double, Bool) Double
-velocity = integralWith bounce (0, False)
--- velocity   (a) Double >>> Double (Double)  =  a Double
+velocity :: Wire DiffTime () IO (Double, Bool) Double
+velocity = integralWith bounce 0
+    where bounce collisions v | collisions = -v
+                              | otherwise  = v
 
 
-bounce :: (w -> a -> a) -> a
-bounce w a = a
+position :: Wire DiffTime () IO Double (Double, Bool)
+position = integralWith'' (clamp 50 150) 100
 
 
-integralWith' :: (Fractional a, HasTime t s) => (a -> a) -> a -> Wire s e m a a
-integralWith' correct = loop'
-    where
-    loop' x' =
+clamp :: (Ord a) => a -> a -> a -> (a, Bool)
+clamp lower upper x
+    | lower > upper     = clamp upper lower x
+    | x < lower         = (lower, True)
+    | x > upper         = (upper, True)
+    | otherwise         = (x, False)
+
+
+integralWith'' :: (Fractional a, HasTime t s) => (a -> (a, o)) -> a -> Wire s e m a (a, o)
+integralWith'' correct = loop'
+  where
+    loop' x =
         mkPure $ \ds dx ->
             let dt = realToFrac (dtime ds)
-                x = correct (x' + dt * dx)
-            in x' `seq` (Right x', loop' x)
+                (x', b) = correct (x + dt * dx)
+            in x `seq` (Right (x, b), loop' x')
 
 
-position :: forall a. Wire DiffTime () IO Double (Double, Bool)
-position = mkSF_ $ \x ->
-    if x > 200 then (200, True) else (x, False)
-
-
-input :: forall a. Wire DiffTime () IO a Double
+input :: Wire DiffTime () IO a Double
 input = proc x -> do
     accel <- acceleration -< x
     rec (position, collides) <- position -< velocity
         velocity <- velocity -< (accel, collides)
     returnA -< position
-
-
 
 
 main :: IO ()
